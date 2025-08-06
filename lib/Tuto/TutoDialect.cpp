8816695f1cd7588f3e213fb12bef04b4d7a698fc
at0m741 <louistouz@icloud.com>
2025-08-06 11:26:16 +0200

    Update TutoDialect.cpp

--- lib/Tuto/TutoDialect.cpp
+++ lib/Tuto/TutoDialect.cpp
@@ -1,12 +1,10 @@
 
 #include "TutoDialect/TutoDialect.h"
 #include "TutoDialect/TutoOps.h"
-
 #include "mlir/IR/DialectImplementation.h"
 
 using namespace mlir;
 using namespace mlir::tuto;
-
 
 void TutoDialect::initialize() {
   addOperations<
